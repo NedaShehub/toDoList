43f4a5d3ac87b73b5b42d6afa4c240159292193e
Neda Shehub <nedashehuh@gmail.com>
2022-06-28 08:37:57 -0700

    Initial Commit

---
+++ toDoList/ToDo.swift
@@ -0,0 +1,13 @@
+//
+//  ToDo.swift
+//  toDoList
+//
+//  Created by Ismael Shehub on 6/27/22.
+//
+
+import UIKit
+
+class ToDo {
+    var name = ""
+    var important = false
+}
---
+++ toDoList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  toDoList
+//
+//  Created by Ismael Shehub on 6/27/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
